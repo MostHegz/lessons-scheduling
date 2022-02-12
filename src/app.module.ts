9d9b3d3dcbe55d91edff9a2ec5097b3d5f3d9fd7
Mustafa Abdelsalam <m.abdelsalam@obeikan.com.sa>
2022-02-12 09:59:56 +0200

    implementing error exception filter to unify error response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Constants } from './common/constants';
 import { LessonsModule } from './module/lessons/lessons.module';
+import { AllExceptionsFilter } from './utilities';
 
 @Module({
   imports: [MongooseModule.forRoot(Constants.MONGODB_CONNECTION), LessonsModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    }
+  ],
 })
 export class AppModule { }
